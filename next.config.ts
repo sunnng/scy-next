f6ffce58f253e82b965e44c35abbe37980f9ef96
Scy <chengyuan7766@gmail.com>
2025-03-21 15:31:50 +0800

    settings.json / biome.json update

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,19 @@
+import { fileURLToPath } from "node:url";
+import createJiti from "jiti";
 import type { NextConfig } from "next";
+
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+// Import env here to validate during build. Using jiti@^1 we can import .ts files :)
+jiti("./src/env/server.ts");
+jiti("./src/env/client.ts");
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  eslint: {
-    // 禁用 Next.js 内置的 ESLint
-    ignoreDuringBuilds: true
-  }
+	eslint: {
+		// 禁用 Next.js 内置的 ESLint
+		ignoreDuringBuilds: true,
+	},
 };
 
 export default nextConfig;
