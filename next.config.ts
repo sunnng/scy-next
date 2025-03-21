05fd02bac77a0e6c2004be21411ea086335e5d21
Scy <chengyuan7766@gmail.com>
2025-03-21 11:11:21 +0800

    biome,husky,lint-stage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 禁用 Next.js 内置的 ESLint
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
